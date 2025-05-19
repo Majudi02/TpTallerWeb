6e13ab156d8408405bb501885f4e5e0909f43370
Mateo <mateodinu02@gmail.com>
2025-05-19 19:04:50 -0300

    agrego la pantalla de hacer un pedido

---
+++ src/main/java/com/tallerwebi/presentacion/PedidoControlador.java
@@ -0,0 +1,41 @@
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.PedidoService;
+import com.tallerwebi.dominio.PedidoServiceImpl;
+import org.dom4j.rule.Mode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PedidoControlador {
+
+    private PedidoService pedidoService;
+
+    @Autowired
+    public PedidoControlador(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+
+    @GetMapping("/pedido")
+    public ModelAndView irAPedido() {
+        ModelMap modeloMap = new ModelMap();
+        modeloMap.put("restaurantes", pedidoService.traerRestaurantesDestacados());
+        modeloMap.put("platos", pedidoService.traerPlatosDestacados());
+        return new ModelAndView("hacer-pedido-home",modeloMap);
+    }
+
+    /*
+
+    @GetMapping("/pedido/restaurantes")
+    public ModelAndView irARestaurantes() {
+        ModelMap modeloMap = new ModelMap();
+        return new ModelAndView("hacer-pedidio-restaurante",modeloMap);
+    }
+
+     */
+}
