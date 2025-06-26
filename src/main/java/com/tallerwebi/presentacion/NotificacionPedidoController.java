5fec69cbf99b813150d01a6b8bc79d623945a257
Mateo <mateodinu02@gmail.com>
2025-06-26 13:26:28 -0300

    arreglo los pedidos

---
+++ src/main/java/com/tallerwebi/presentacion/NotificacionPedidoController.java
@@ -0,0 +1,40 @@
+package com.tallerwebi.presentacion;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@RestController
+public class NotificacionPedidoController {
+
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+    @GetMapping("/restaurante/notificaciones-pedidos")
+    public SseEmitter conectarRestaurante() {
+        SseEmitter emitter = new SseEmitter(0L);
+        emitters.add(emitter);
+
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+        emitter.onError(e -> emitters.remove(emitter));
+
+        return emitter;
+    }
+
+    public void notificarMensaje(String mensaje) {
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("nuevo-plato")
+                        .data(mensaje));
+            } catch (IOException e) {
+                emitters.remove(emitter);
+            }
+        }
+    }
+}
--- src/test/java/com/tallerwebi/punta_a_punta/VistaLoginE2E.java
+++ src/test/java/com/tallerwebi/punta_a_punta/VistaLoginE2E.java
@@ -2,16 +2,22 @@ package com.tallerwebi.punta_a_punta;
 
 import com.microsoft.playwright.*;
 import com.tallerwebi.punta_a_punta.vistas.VistaLogin;
+import com.tallerwebi.punta_a_punta.vistas.VistaNuevoUsuario;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
 public class VistaLoginE2E {
+
 
     static Playwright playwright;
     static Browser browser;
@@ -22,8 +28,7 @@ public class VistaLoginE2E {
     static void abrirNavegador() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch();
-        //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
-
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(500));
     }
 
     @AfterAll
@@ -33,6 +38,8 @@ public class VistaLoginE2E {
 
     @BeforeEach
     void crearContextoYPagina() {
+        ReiniciarDB.limpiarBaseDeDatos();
+
         context = browser.newContext();
         Page page = context.newPage();
         vistaLogin = new VistaLogin(page);
@@ -44,26 +51,72 @@ public class VistaLoginE2E {
     }
 
     @Test
-    void deberiaDecirUNLAMEnElNavbar() {
+    void deberiaDecirUNLAMEnElNavbar() throws MalformedURLException {
+        dadoQueElUsuarioEstaEnLaVistaDeLogin();
+        entoncesDeberiaVerUNLAMEnElNavbar();
+    }
+
+    @Test
+    void deberiaDarUnErrorAlIntentarIniciarSesionConUnUsuarioQueNoExiste() {
+        dadoQueElUsuarioCargaSusDatosDeLoginCon("damian@unlam.edu.ar", "unlam");
+        cuandoElUsuarioTocaElBotonDeLogin();
+        entoncesDeberiaVerUnMensajeDeError();
+    }
+
+    @Test
+    void deberiaNavegarAlHomeSiElUsuarioExiste() throws MalformedURLException {
+        dadoQueElUsuarioCargaSusDatosDeLoginCon("test@unlam.edu.ar", "test");
+        cuandoElUsuarioTocaElBotonDeLogin();
+        entoncesDeberiaSerRedirigidoALaVistaDeHome();
+    }
+
+    @Test
+    void deberiaRegistrarUnUsuarioEIniciarSesionExistosamente() throws MalformedURLException {
+    //    dadoQueElUsuarioNavegaALaVistaDeRegistro();
+        dadoQueElUsuarioSeRegistraCon("juan@unlam.edu.ar", "123456");
+        dadoQueElUsuarioEstaEnLaVistaDeLogin();
+        dadoQueElUsuarioCargaSusDatosDeLoginCon("juan@unlam.edu.ar", "123456");
+        cuandoElUsuarioTocaElBotonDeLogin();
+        entoncesDeberiaSerRedirigidoALaVistaDeHome();
+    }
+
+    private void entoncesDeberiaVerUNLAMEnElNavbar() {
         String texto = vistaLogin.obtenerTextoDeLaBarraDeNavegacion();
         assertThat("UNLAM", equalToIgnoringCase(texto));
     }
 
-    @Test
-    void deberiaDarUnErrorAlNoCompletarElLoginYTocarElBoton() {
-        vistaLogin.escribirEMAIL("damian@unlam.edu.ar");
-        vistaLogin.escribirClave("unlam");
+    private void dadoQueElUsuarioEstaEnLaVistaDeLogin() throws MalformedURLException {
+        URL urlLogin = vistaLogin.obtenerURLActual();
+        assertThat(urlLogin.getPath(), matchesPattern("^/login(?:;jsessionid=[^/\\s]+)?$"));
+    }
+
+    private void cuandoElUsuarioTocaElBotonDeLogin() {
         vistaLogin.darClickEnIniciarSesion();
+    }
+
+    private void entoncesDeberiaSerRedirigidoALaVistaDeHome() throws MalformedURLException {
+        URL url = vistaLogin.obtenerURLActual();
+        assertThat(url.getPath(), matchesPattern("^/home(?:;jsessionid=[^/\\s]+)?$"));
+    }
+
+    private void entoncesDeberiaVerUnMensajeDeError() {
         String texto = vistaLogin.obtenerMensajeDeError();
         assertThat("Error Usuario o clave incorrecta", equalToIgnoringCase(texto));
     }
 
-    @Test
-    void deberiaNavegarAlHomeSiElUsuarioExiste() {
-        vistaLogin.escribirEMAIL("test@unlam.edu.ar");
-        vistaLogin.escribirClave("test");
-        vistaLogin.darClickEnIniciarSesion();
-        String url = vistaLogin.obtenerURLActual();
-        assertThat(url, containsStringIgnoringCase("/spring/home"));
+    private void dadoQueElUsuarioCargaSusDatosDeLoginCon(String email, String clave) {
+        vistaLogin.escribirEMAIL(email);
+        vistaLogin.escribirClave(clave);
+    }
+
+//    private void dadoQueElUsuarioNavegaALaVistaDeRegistro() {
+//        vistaLogin.darClickEnRegistrarse();
+//    }
+
+    private void dadoQueElUsuarioSeRegistraCon(String email, String clave) {
+        VistaNuevoUsuario vistaNuevoUsuario = new VistaNuevoUsuario(context.pages().get(0));
+        vistaNuevoUsuario.escribirEMAIL(email);
+        vistaNuevoUsuario.escribirClave(clave);
+        vistaNuevoUsuario.darClickEnRegistrarme();
     }
 }
